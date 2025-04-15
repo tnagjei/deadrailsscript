91375230c248044f90ee926a9622168825ae1b4e
唐杰 <tangjei@gmail.com>
2025-04-15 06:56:38 +0000

    接着刚才文本列表，注意，不是替换，是接着列表最下方

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,6 +31,11 @@ const scriptList = [
     codeSnippet: 'loadstring(game:HttpGet("https://api.luarmor.net/f...',
     fullCode: 'This is the full code for Best script op'
   },
+    {
+    name: "Dead Rails Script SpeedHubX - Auto Bond, Auto Finish Game, ESP",
+    codeSnippet: 'loadstring(game:HttpGet("https://raw.githubusercon...',
+    fullCode: 'This is the full code for Dead Rails Script SpeedHubX - Auto Bond, Auto Finish Game, ESP'
+  },
 ];
 
 const ScriptItem = ({ name, codeSnippet, fullCode }: { name: string; codeSnippet: string; fullCode: string }) => {
